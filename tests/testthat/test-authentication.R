796f403bafd8b415115b5f18118d3ca62bb82b2b
default <default@default.com>
2020-04-26 17:22:21 -0300

    test create token

---
+++ tests/testthat/test-authentication.R
@@ -0,0 +1,12 @@
+test_that("load works", {
+
+    generate_token("sample_settings.json")
+
+    latest = readRDS("rtweet_token.rds")
+    test = readRDS("test_rtweet_token.rds")
+
+    expect_true(all.equal(latest, test))
+
+    file.remove("rtweet_token.rds")
+
+})
